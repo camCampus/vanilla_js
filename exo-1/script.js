6a683ae769d57682e9363c1a40418ee802bfae74
camCampus <camille.bochar@le-campus-numerique.fr>
2023-01-11 10:10:18 +0100

    exo 1 done

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,11 @@
 // you can write js here
-console.log('hello from file');
+
+let kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+
+//Convertion des kelvins en celsius
+let celsius = kelvins - 273;
+console.log("cel= " + celsius);
+//Convertion des celsius en fahrenheits et arrondis inferieur ac MAth.floor()
+let fahrenheits = celsius * (9 / 5) + 32;
+fahrenheits = Math.floor(fahrenheits);
+console.log("far= " + fahrenheits);
