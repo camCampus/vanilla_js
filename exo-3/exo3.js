0d0a65fa633003174b0005a2b337c6601766afb7
camCampus <camille.bochar@le-campus-numerique.fr>
2023-01-11 12:17:12 +0100

    Exo 3 done

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,2 +1,74 @@
 // you can write js here
-console.log('exo-3');
+let playerInput = prompt("rock, paper, scissors");
+
+let choice = ["rock", "paper", "scissors", "bomb"];
+//let playerInput = choice[Math.floor(Math.random() * 3)];
+
+function getPlayerChoice(userInput) {
+  let u = userInput.toLowerCase();
+
+  for (i = 0; i < choice.length; i++) {
+    if (u === choice[i]) {
+      return u;
+    }
+  }
+  return console.log("error");
+}
+
+function getComputerChoice() {
+  let r = Math.floor(Math.random() * 3);
+  return choice[r];
+}
+
+function findWinner(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    return console.log("Egalité");
+  }
+  console.log("You play: " + playerChoice);
+  console.log("computer play: " + computerChoice);
+  let a;
+  if (playerChoice === "bomb") {
+    return console.log("WIN cheater =)");
+  }
+
+  if (playerChoice === "rock") {
+    switch (computerChoice) {
+      case "paper":
+        a = "Lost";
+        break;
+      case "scissors":
+        a = "Win";
+        break;
+    }
+  }
+  if (playerChoice === "paper") {
+    switch (computerChoice) {
+      case "scissors":
+        a = "Lost";
+        break;
+      case "rock":
+        a = "Win";
+        break;
+    }
+  }
+  if (playerChoice === "scissors") {
+    switch (computerChoice) {
+      case "paper":
+        a = "Lost";
+        break;
+      case "rock":
+        a = "Win";
+        break;
+    }
+  }
+  return console.log(a);
+}
+
+function playGame() {
+  let user = getPlayerChoice(playerInput);
+  let computer = getComputerChoice();
+
+  findWinner(user, computer);
+}
+
+playGame();
