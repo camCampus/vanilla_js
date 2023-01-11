dcfc836ea2d5e35e3fc9a958479e695e611b9283
camCampus <camille.bochar@le-campus-numerique.fr>
2023-01-11 14:24:34 +0100

    exo 5 done

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,16 @@
 // you can write js here
 
-console.log('exo-5');
+let input = prompt("Traduire en baleine");
+let vowels = ["a", "e", "i", "o", "u", "y"];
+let resultArray = [];
+
+let t = input.split("");
+t.forEach((e, index) => {
+  for (i = 0; i < vowels.length; i++) {
+    if (e === vowels[i]) {
+      resultArray.push(e);
+    }
+  }
+});
+let res = resultArray.join().toUpperCase();
+console.log(res);
