5a56bd55edc7192f1f645af09140e59f1edfb4c9
camCampus <camille.bochar@le-campus-numerique.fr>
2023-01-11 13:59:53 +0100

    exo 4 done

--- exo-4/script.js
+++ exo-4/script.js
@@ -1,5 +1,61 @@
 // you can write js here
 
-console.log('exo-4');
+console.log("exo-4");
 
-var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+var secretMessage = [
+  "Learning",
+  "isn't",
+  "about",
+  "what",
+  "you",
+  "get",
+  "easily",
+  "the",
+  "first",
+  "time,",
+  "it's",
+  "about",
+  "what",
+  "you",
+  "can",
+  "figure",
+  "out.",
+  "-2015,",
+  "Chris",
+  "Pine,",
+  "Learn",
+  "JavaScript",
+];
+
+let last = secretMessage.pop();
+console.log(secretMessage);
+
+let addTo = secretMessage.push("to");
+let addProgram = secretMessage.push("program");
+console.log(secretMessage);
+
+/*
+for (i = 0; i < secretMessage.length; i++) {
+  if (secretMessage[i] === "easily") {
+    secretMessage[i] = "rigth";
+  }
+}
+*/
+secretMessage.forEach((item, index) => {
+  if (item === "easily") {
+    secretMessage[index] = "rigth";
+  }
+});
+console.log(secretMessage);
+
+let first = secretMessage.shift();
+console.log(secretMessage);
+
+let f = secretMessage.unshift("Programming");
+console.log(secretMessage);
+
+secretMessage = secretMessage.map((element) =>
+  element.replace(/get|right|the|first|time/g, "know")
+);
+console.log(secretMessage);
+console.log(secretMessage.join());
---
+++ exoComp.js
@@ -0,0 +1,32 @@
+//exo 1
+console.log("exo 1");
+
+let num = 255;
+let txt = "255";
+let bin = true;
+let flt = 7.23;
+
+//exo 2
+console.log("exo 2");
+num++;
+console.log(num);
+txt = txt + " est ici une chaîne de caractères";
+console.log(txt);
+bin = false;
+console.log(bin);
+flt = flt + num;
+console.log(flt);
+
+//exo 3
+console.log("exo 3");
+num = 23;
+txt = "33";
+console.log(num + txt);
+let r = num + Number(txt);
+console.log("r: " + r);
+
+//exo 4
+let tmp = 0;
+tmp = parseInt(txt);
+tmp = tmp + num;
+console.log(tmp);
