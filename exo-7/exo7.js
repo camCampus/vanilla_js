67d458ec7c4be82098036a3ffe5e4cac16d9bbb3
camCampus <camille.bochar@le-campus-numerique.fr>
2023-01-16 12:25:34 +0100

    Sort done

--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -23,21 +23,25 @@ function init() {
   //Pour chaque obj de data creation d'un li pour affficher l'ojt dans le DOM
   data.forEach((e) => {
     let li = document.createElement("li");
+    li.style.display = "block";
     li.innerHTML = JSON.stringify(e);
     ul.appendChild(li);
   });
 }
 
-//Retourn un tableau des objets affichés
+//Retourne un tableau des objets affichés
 function getDisplayObject() {
   let k = Array.from(ul.children);
   let objDisplay = [];
   k.forEach((e) => {
-    let txt = JSON.parse(e.textContent);
-    objDisplay.push(txt);
+    if (e.getAttribute("style") === "display: block;") {
+      let txt = JSON.parse(e.textContent);
+      objDisplay.push(txt);
+    }
   });
   return objDisplay;
 }
+
 //Permet de filtrer les produits par types et rupture de stock
 function showType() {
   let val = document.getElementById("inputType").value;
@@ -45,6 +49,7 @@ function showType() {
   let k = Array.from(ul.children);
   let show = [];
   let displayObj = getDisplayObject();
+
   //Reset affichage pour une autre recherche
   k.forEach((e) => {
     e.style.display = "block";
@@ -56,7 +61,6 @@ function showType() {
       show.push(k[i]);
     }
     if (check.checked === true) {
-      console.log("ok");
       if (e.quantity === 0) {
         show.push(k[i]);
       }
@@ -68,8 +72,9 @@ function showType() {
 }
 
 //Fonction pour filtrer les objets en fonction de le clefs, de la valeur et pour operator c'est ascendant ou descendant
-function sort(val, keys, operator) {
-  val.sort((a, b) => {
+function sortDisplayObj(keys, operator) {
+  let data = getDisplayObject();
+  data.sort((a, b) => {
     switch (operator) {
       case "asc":
         if (a[keys] < b[keys]) return -1;
@@ -83,27 +88,45 @@ function sort(val, keys, operator) {
         return 0;
     }
   });
+  return data;
 }
 
 //Function pour recuperer les options de filtres de l'utilisateur et les passer à la fonction de trie
 function productFilter() {
-  let option = document.getElementById("filter");
-  let ascdesc = document.getElementById("ascdesc");
+  let filterOption1 = document.getElementById("filter");
+  let filterOption2 = document.getElementById("ascdesc");
+  let key;
+  let newDisplay;
 
-  let fil = option.addEventListener("change", (event) => {
-    fil = event.target.value;
-
-    let echo = sort(getDisplayObject(), fil, AD);
+  filterOption1.addEventListener("change", (event) => {
+    key = event.target.value;
+    if (operator === undefined) {
+    } else {
+      newDisplay = sortDisplayObj(key, operator);
+      ul.innerHTML = "";
+      newDisplay.forEach((e) => {
+        let li = document.createElement("li");
+        li.innerHTML = JSON.stringify(e);
+        li.style.display = "block";
+        ul.appendChild(li);
+      });
+    }
   });
-  let AD = ascdesc.addEventListener("change", (event) => {
-    AD = event.target.value;
-    sort(data, fil, AD);
+
+  let operator = filterOption2.addEventListener("change", (event) => {
+    operator = event.target.value;
+    newDisplay = sortDisplayObj(key, operator);
+    ul.innerHTML = "";
+    newDisplay.forEach((e) => {
+      let li = document.createElement("li");
+      li.innerHTML = JSON.stringify(e);
+      li.style.display = "block";
+      ul.appendChild(li);
+    });
   });
 }
-
 // Bloc de code pour récupérer les data du form et ajouter un obj à la liste
 let myForm = document.getElementById("myForm");
-console.log(myForm);
 
 myForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -114,9 +137,9 @@ myForm.addEventListener("submit", (event) => {
   data.push(newObj);
   ul.innerHTML = "";
   init();
-  console.log(newObj);
 });
 
 init();
 productFilter();
+
 // fetchForm().then((r) => console.log(r));
